334d59bb86ceb09e808aeddfd4a2863d14a15f4f
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-20 17:43:39 +0700

    feat: add variety prisma repository

---
+++ src/infrastructure/database/repositories/prisma-variety.repository.ts
@@ -0,0 +1,39 @@
+import type { Prisma } from '../../../../prisma/generated/prisma/client.js';
+
+import { PrismaBaseRepository } from './prisma-base.repository.js';
+
+export class PrismaVarietyRepository extends PrismaBaseRepository {
+  findById(id: string) {
+    return this.execute(() => this.prisma.variety.findUnique({ where: { id } }));
+  }
+
+  findByUuid(uuid: string) {
+    return this.execute(() => this.prisma.variety.findUnique({ where: { uuid } }));
+  }
+
+  findMany(args?: Prisma.VarietyFindManyArgs) {
+    return this.execute(() => this.prisma.variety.findMany(args));
+  }
+
+  count(args?: Prisma.VarietyCountArgs) {
+    return this.execute(() => this.prisma.variety.count(args));
+  }
+
+  create(data: Prisma.VarietyCreateInput) {
+    return this.executeTransaction((transaction) =>
+      transaction.variety.create({ data }),
+    );
+  }
+
+  update(where: Prisma.VarietyWhereUniqueInput, data: Prisma.VarietyUpdateInput) {
+    return this.executeTransaction((transaction) =>
+      transaction.variety.update({ where, data }),
+    );
+  }
+
+  delete(where: Prisma.VarietyWhereUniqueInput) {
+    return this.executeTransaction((transaction) =>
+      transaction.variety.delete({ where }),
+    );
+  }
+}
