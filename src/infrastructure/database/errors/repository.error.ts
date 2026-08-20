6001e480b0d689e74e5b9e495682fb77cd51aefb
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-20 17:42:43 +0700

    feat: add master data repository errors

---
+++ src/infrastructure/database/errors/repository.error.ts
@@ -0,0 +1,32 @@
+export abstract class RepositoryError extends Error {
+  abstract readonly code: string;
+
+  protected constructor(message: string, options?: { cause?: unknown }) {
+    super(message, options);
+    this.name = this.constructor.name;
+  }
+}
+
+export class RepositoryUniqueConstraintError extends RepositoryError {
+  readonly code = 'REPOSITORY_UNIQUE_CONSTRAINT';
+
+  constructor(public readonly fields: string[], cause?: unknown) {
+    super(`Unique constraint violated for ${fields.join(', ')}`, { cause });
+  }
+}
+
+export class RepositoryNotFoundError extends RepositoryError {
+  readonly code = 'REPOSITORY_NOT_FOUND';
+
+  constructor(cause?: unknown) {
+    super('Repository entity was not found', { cause });
+  }
+}
+
+export class RepositoryPersistenceError extends RepositoryError {
+  readonly code = 'REPOSITORY_PERSISTENCE_FAILURE';
+
+  constructor(cause?: unknown) {
+    super('Repository persistence operation failed', { cause });
+  }
+}
