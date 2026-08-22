71e356fe186e7995d33afd72cc28731a03aab566
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2026-08-22 12:56:32 +0700

    fix: correct master data use-case service import

--- src/application/master-data/use-cases/master-data-crud.use-case.ts
+++ src/application/master-data/use-cases/master-data-crud.use-case.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 import type {
   MasterDataEntityName,
   MasterDataListQuery,
+  MasterDataRecord,
+  MasterDataRepositoryFactory,
   MasterDataWrite,
 } from '../../../domain/shared/repositories/master-data.repository.js';
-import { MasterDataCrudService } from './master-data-crud.service.js';
+import { MasterDataCrudService } from '../services/master-data-crud.service.js';
 
 export interface CrudRequest {
   id?: string;
@@ -14,23 +16,42 @@ export interface CrudRequest {
 @Injectable()
 export class MasterDataCrudUseCase {
   constructor(protected readonly service: MasterDataCrudService) {}
-  create(entity: MasterDataEntityName, input: MasterDataWrite) {
+
+  create(
+    entity: MasterDataEntityName,
+    input: MasterDataWrite,
+  ): ReturnType<MasterDataRepositoryFactory['get']> extends infer _T
+    ? Promise<MasterDataRecord>
+    : never {
     return this.service.create(entity, input);
   }
-  get(entity: MasterDataEntityName, identifier: CrudRequest) {
+
+  get(
+    entity: MasterDataEntityName,
+    identifier: CrudRequest,
+  ): Promise<MasterDataRecord> {
     return this.service.get(entity, identifier);
   }
-  list(entity: MasterDataEntityName, query?: MasterDataListQuery) {
+
+  list(
+    entity: MasterDataEntityName,
+    query?: MasterDataListQuery,
+  ): ReturnType<MasterDataCrudService['list']> {
     return this.service.list(entity, query);
   }
+
   update(
     entity: MasterDataEntityName,
     identifier: CrudRequest,
     input: MasterDataWrite,
-  ) {
+  ): ReturnType<MasterDataCrudService['update']> {
     return this.service.update(entity, identifier, input);
   }
-  delete(entity: MasterDataEntityName, identifier: CrudRequest) {
+
+  delete(
+    entity: MasterDataEntityName,
+    identifier: CrudRequest,
+  ): ReturnType<MasterDataCrudService['delete']> {
     return this.service.delete(entity, identifier);
   }
 }
